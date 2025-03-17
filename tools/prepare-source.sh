2cf95d1947dcc9902a2314cedfde809a4346c7ce
Lukas Zapletal <lzap+git@redhat.com>
2025-03-17 18:02:07 +0100

    build: run 'go tidy' as the last prep step

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -3,8 +3,6 @@ set -eu
 
 GO_MAJOR_VER=1.22
 GO_VERSION=1.22.9 # also update .github/workflows/tests.yml
-OAPI_VERSION=2.4.1
-TOOLS_PATH="$(realpath "$(dirname "$0")/bin")"
 
 # Check latest Go version for the minor we're using
 LATEST=$(curl -s https://endoflife.date/api/go/"${GO_MAJOR_VER}".json  | jq -r .latest)
@@ -12,27 +10,15 @@ if test "$LATEST" != "$GO_VERSION"; then
     echo "WARNING: A new minor release is available (${LATEST}), consider bumping the project version (${GO_VERSION})"
 fi
 
-set -x
-export GOTOOLCHAIN=go$GO_VERSION
-export GOSUMDB='sum.golang.org' # this is turned off for Go from Fedora / RHEL
-go version
-
 # Pin Go and toolchain versions at a reasonable version
 go get go@$GO_VERSION toolchain@$GO_VERSION
 
-# Update go.mod and go.sum:
-go mod tidy
+# Generate source
+go generate -x ./cmd/... ./internal/...
 
-# Make sure dev tools are in go.mod
-go get github.com/oapi-codegen/oapi-codegen/v2@v$OAPI_VERSION
-
-# Ensure dev tools are installed
-test -e "$TOOLS_PATH/goimports" || GOBIN=$TOOLS_PATH go install golang.org/x/tools/cmd/goimports@v0.30.0
-("$TOOLS_PATH/oapi-codegen" -version | grep "$OAPI_VERSION" >/dev/null) || GOBIN=$TOOLS_PATH go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v$OAPI_VERSION
-
-# Generate source (skip vendor/):
-GOBIN=$TOOLS_PATH go generate -x ./cmd/... ./internal/...
-
-# Reformat source (skip vendor/):
-"$TOOLS_PATH/goimports" -w ./internal ./cmd
+# Reformat source
+go run golang.org/x/tools/cmd/goimports@latest -w ./internal ./cmd
 go fmt ./cmd/... ./internal/...
+
+# Update go.mod and go.sum (keep it as the last)
+go mod tidy
