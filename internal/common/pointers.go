3adf5b04f4d8b60e360d8ca2fe6c8ce85e39f93d
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-03-28 14:58:56 +0100

    common: add DerefOrDefault helper function

--- internal/common/pointers.go
+++ internal/common/pointers.go
@@ -10,3 +10,13 @@ func FromPtr[T any](ref *T) (value T) {
 	}
 	return
 }
+
+// DerefOrDefault returns the dereferenced value of the given pointer or the
+// default value for the type if unset.
+func DerefOrDefault[T any](p *T) T {
+	var v T
+	if p != nil {
+		v = *p
+	}
+	return v
+}
