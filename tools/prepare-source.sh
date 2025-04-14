6d79ace144402fb355906861d89d82b4a4d9c992
Sanne Raymaekers <sanne.raymaekers@gmail.com>
2025-04-14 15:33:59 +0200

    tools/prepare-source: remove go version check
    We always want to be using the version matching whatever is in the ubi9
    go-toolset container.

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -4,12 +4,6 @@ set -eu
 GO_MAJOR_VER=1.22
 GO_VERSION=1.22.9 # also update .github/workflows/tests.yml
 
-# Check latest Go version for the minor we're using
-LATEST=$(curl -s https://endoflife.date/api/go/"${GO_MAJOR_VER}".json  | jq -r .latest)
-if test "$LATEST" != "$GO_VERSION"; then
-    echo "WARNING: A new minor release is available (${LATEST}), consider bumping the project version (${GO_VERSION})"
-fi
-
 # Pin Go and toolchain versions at a reasonable version
 go get go@$GO_VERSION toolchain@$GO_VERSION
 
