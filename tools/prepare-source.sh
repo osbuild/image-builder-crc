7136b89ce265d0387270b8f184fa9d65c6650c30
Lukas Zapletal <lzap+git@redhat.com>
2026-07-29 15:20:41 +0200

    chore: upgrade Go to 1.26 + OpenAPI

--- tools/prepare-source.sh
+++ tools/prepare-source.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 set -eu
 
-GO_VERSION=1.24.12 # also update .github/workflows/tests.yml
+GO_VERSION=1.26.5 # also update .github/workflows/tests.yml
 
 # Pin Go and toolchain versions at a reasonable version
 go get go@$GO_VERSION toolchain@$GO_VERSION
+go mod tidy
 
 # Generate source
 go generate -x ./cmd/... ./internal/...
 
+# Update go.mod and go.sum
+go mod tidy
+
 # Reformat source
 go run golang.org/x/tools/cmd/goimports@latest -w ./internal ./cmd
 go fmt ./cmd/... ./internal/...
-
-# Update go.mod and go.sum (keep it as the last)
-go mod tidy
